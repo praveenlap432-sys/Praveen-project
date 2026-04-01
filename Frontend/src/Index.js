aa46a5f9dd119c94d70c8c52e26801f80140bd72
praveenlap432-sys <praveenlp432@gmail.com>
2026-04-01 11:36:55 +0530

    Frontend/src/Index.js

---
+++ Frontend/src/Index.js
@@ -0,0 +1,54 @@
+// let arr = ['apple','orange','mango']
+
+// // for(let a =0; a<arr.length;a++){
+// //     console.log(arr[a])
+// // }
+
+// for(let fruit of arr){
+//     console.log(fruit)
+// }
+
+
+// let obj = {
+//     name:'praveen',
+//     age:'21',
+//     native:'salem'
+// }
+
+// for (let key in obj){
+//     // console.log(key)
+//     console.log(obj[key])
+// }
+
+
+
+
+// let kl = ['1','2','3']
+
+
+
+
+
+// // for(let a = 0 ;a<=kl.length;a++){
+// //     console.log(kl[a])
+// // }
+
+
+// for(let fruits of kl){
+//     console.log(  fruits)
+// }
+
+
+
+// let nk = {
+//     name:'klm',
+//     age:'klr',
+//     native:'cbe'
+// }
+
+
+// for(let key in nk){
+//     console.log(key)
+//     console.log(nk[key])
+// }
+
